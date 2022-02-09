c7423509e1934e56817106008bcfa0152371c116
Hanbyeol <hbstella92@gmail.com>
2022-02-09 17:32:58 +0900

    LeetCode 669

---
+++ LeetCode/TrimaBinarySearchTree.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+	TreeNode* trimBST(TreeNode* root, int low, int high) {
+		if (!root) return nullptr;
+
+		if (root->val < low) return trimBST(root->right, low, high);
+		if (root->val > high) return trimBST(root->left, low, high);
+
+		root->left = trimBST(root->left, low, high);
+		root->right = trimBST(root->right, low, high);
+		return root;
+	}
+};
+
+int main() {
+	Solution sol;
+	TreeNode *root;
+	TreeNode node[10];
+	int arr[10] = { 1,0,2 };
+
+	for (int i = 0; i < 3; i++) {
+		node[i].val = arr[i];
+	}
+
+	root = &node[0];
+	root->left = &node[1];
+	root->right = &node[2];
+
+	root = sol.trimBST(root, 1, 2);
+
+	return 0;
+}
