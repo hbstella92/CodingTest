6da1009c2a32d77b37a14ad157480c15e98fb7c9
Hanbyeol <hbstella92@gmail.com>
2022-02-09 17:08:15 +0900

    LeetCode 1689

---
+++ LeetCode/PartitioningIntoMinimumNumberOfDeciBinaryNumbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int minPartitions(string n) {
+		int n_len = n.length();
+		int maxval = 0;
+
+		for (int i = 0; i < n_len; i++) {
+			if (maxval == 9) break;
+			maxval = (maxval < (n[i] - '0')) ? (n[i] - '0') : maxval;
+		}
+
+		return maxval;
+	}
+};
+
+int main() {
+	Solution sol;
+
+	cout << sol.minPartitions("32") << endl; // 3
+
+	return 0;
+}
