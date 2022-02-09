bb0f34aca0daee9071844b4b1b27e0ff03031a30
Hanbyeol <hbstella92@gmail.com>
2022-02-09 17:08:40 +0900

    LeetCode 1769

---
+++ LeetCode/MinimumNumberofOperationstoMoveAllBallstoEachBox.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> minOperations(string boxes) {
+		int b_len = boxes.length();
+		vector<int> ans;
+		vector<int> idx;
+
+		for (int i = 0; i < b_len; i++) {
+			if (boxes[i] == '1') {
+				idx.push_back(i);
+			}
+		}
+
+		int idx_sz = idx.size();
+
+		for (int i = 0; i < b_len; i++) {
+			int sum = 0;
+
+			for (int j = 0; j < idx_sz; j++) {
+				sum += abs(i - idx[j]);
+			}
+
+			ans.push_back(sum);
+		}
+
+		return ans;
+	}
+};
+
+int main() {
+	Solution sol;
+	vector<int> ans = sol.minOperations("110");
+	int ans_sz = ans.size();
+
+	for (int i = 0; i < ans_sz; i++) {
+		cout << ans[i] << " ";
+	} cout << endl;
+
+	return 0;
+}
