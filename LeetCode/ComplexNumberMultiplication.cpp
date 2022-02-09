9da3f760c00b29c0fbbb1a5ae0cce584a9ad09d1
Hanbyeol <hbstella92@gmail.com>
2022-02-09 17:16:14 +0900

    LeetCode 537

---
+++ LeetCode/ComplexNumberMultiplication.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	string complexNumberMultiply(string num1, string num2) {
+		string ans = "";
+		int n1_len = num1.length(), n2_len = num2.length();
+
+		int idx = num1.find('+');
+		int r1 = stoi(num1.substr(0, idx));
+		int i1 = stoi(num1.substr(idx + 1, n1_len - 1));
+		idx = num2.find('+');
+		int r2 = stoi(num2.substr(0, idx));
+		int i2 = stoi(num2.substr(idx + 1, n2_len - 1));
+
+		int sr = 0, si = 0;
+		sr += (r1 * r2);
+		si += ((r1 * i2) + (r2 * i1));
+		sr += (i1 * i2 * (-1));
+
+		ans += to_string(sr) + '+' + to_string(si) + 'i';
+		return ans;
+	}
+};
+
+int main() {
+	Solution sol;
+
+	cout << sol.complexNumberMultiply("1+1i", "1+1i") << endl; // 0+2i
+
+	return 0;
+}
