90b14d26732a548166eb95c14be325b3114315ba
Hanbyeol <hbstella92@gmail.com>
2022-02-09 17:13:26 +0900

    LeetCode 1529

---
+++ LeetCode/MinimumSuffixFlips.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	int minFlips(string target) {
+		int ans = 0;
+		int len = target.length();
+
+		if (target[0] == '1') ans++;
+		for (int i = 0; i < len - 1; i++) {
+			if (target[i] != target[i + 1]) {
+				ans++;
+			}
+		}
+
+		return ans;
+	}
+};
+
+int main() {
+	Solution sol;
+
+	cout << sol.minFlips("10111") << endl; // 3
+
+	return 0;
+}
