a21e6c8f085c2dd840f0f0ba4ced63288c372da9
Hanbyeol <hbstella92@gmail.com>
2022-02-09 17:05:18 +0900

    LeetCode 28

---
+++ LeetCode/ImplementStrStr.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	int strStr(string haystack, string needle) {
+		if (needle == "") return 0;
+
+		string tmp = "";
+		int h_len = haystack.length();
+		int n_len = needle.length();
+		for (int i = 0; i <= h_len - n_len; i++) {
+			tmp = haystack.substr(i, n_len);
+
+			if (tmp == needle) return i;
+		}
+
+		return -1;
+	}
+};
+
+int main() {
+	Solution sol;
+
+	cout << sol.strStr("hello", "ll") << '\n' // 2
+
+	return 0;
+}
