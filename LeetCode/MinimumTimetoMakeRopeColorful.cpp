5a8d7feaf35a32d8d682fc5a8a3bc503b32f982f
Hanbyeol <hbstella92@gmail.com>
2022-02-26 14:29:39 +0900

    Leetcode 1578

---
+++ LeetCode/MinimumTimetoMakeRopeColorful.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int minCost(string colors, vector<int>& neededTime) {
+		int ans = 0;
+		int len = colors.length();
+
+		int cmp = 0;
+		for (int i = 1; i < len; i++) {
+			if (colors[cmp] == colors[i]) {
+				ans += min(neededTime[cmp], neededTime[i]);
+				cmp = (neededTime[cmp] < neededTime[i]) ? i : cmp;
+			}
+			else cmp = i;
+		}
+
+		return ans;
+	}
+};
+
+int main() {
+	Solution sol;
+	vector<int> input = { 1,2,3,4,5 };
+
+	cout << sol.minCost("abaac", input) << endl; // 3
+
+	return 0;
+}
